c178c356d96b2c8cc0029c44a2afd4d512b7ffae
JacekKorta <jacek.korta@gmail.com>
2022-09-18 10:59:47 +0200

    Add !=200 response handling

--- main.go
+++ main.go
@@ -28,23 +28,32 @@ func main() {
 
 	myClient := &http.Client{Timeout: 10 * time.Second}
 	
-	page := 1
+	page := 26
 	fromDate := 0
 	hasMore := true
+	errorsCount := 0
 
 	for hasMore {
+		if errorsCount > 2 {
+			log.Println("There is some constant problem with API. Closing program...")
+			break
+		}
 		questionsClient := questions.NewClient(myClient)
 		result, err := questionsClient.GetQuestions(settings, page, fromDate)
 
 		if err != nil {
 			log.Println("There is so problem with stackoverflow API")
+			errorsCount++
+			continue
 		}
+		errorsCount = 0
 		for _, item := range result.Items {
 			wg.Add(1)
 			go sendQuestionToQueue(item)
 		}
 		hasMore = result.HasMore
 		page++
+
 	}
 
 	wg.Wait()
